80adf393e4d12a50cb84731cbe233f1ab5cc02a9
wagga40 <6437862+wagga40@users.noreply.github.com>
2025-03-30 15:08:15 +0200

    Back port wmetcalf/FixUpFirstJsonScopeHandling

--- zircolite.py
+++ zircolite.py
@@ -352,6 +352,7 @@ class zirCore:
         self.timeField = timeField
         self.hashes = hashes
         self.delimiter = delimiter
+        self.first_json_output = True  # To manage commas in JSON output
     
     def close(self):
         self.dbConnection.close()
@@ -565,7 +566,6 @@ class zirCore:
         Execute all rules in the ruleset and handle output.
         """
         csvWriter = None
-        first_json_output = True  # To manage commas in JSON output
         is_json_mode = not self.csvMode
 
         # Prepare output file handle if needed
@@ -628,10 +628,10 @@ class zirCore:
                         # Write results as JSON using orjson
                         try:
                             # Handle commas between JSON objects
-                            if not first_json_output:
+                            if not self.first_json_output:
                                 fileHandle.write(',\n')
                             else:
-                                first_json_output = False
+                                self.first_json_output = False
                             # Serialize ruleResults to JSON bytes with indentation
                             json_bytes = json.dumps(ruleResults, option=json.OPT_INDENT_2)
                             # Write the decoded JSON string to the file
